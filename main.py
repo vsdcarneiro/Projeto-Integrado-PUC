1da49066b5d9a6847deb8fc41676cfc178546b97
Vinícius Carneiro <vinixiussouza@gmail.com>
2022-12-15 18:32:32 -0300

    refactor: fine adjustments

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(title='Water Quality')
 @app.post("/predict")
 def predict_water_quality(data: list):
     try:
-        quality = predict_quality([data])
-        return {'quality': str(quality[0])}
+        quality = predict_quality(data)
+        return {'quality': str(quality)}
     except Exception:
         return {'error': 'It was not possible to classify the water quality.'}
