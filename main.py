e5b7eecd315df17dd9d15751f77dceadc957f130
Vinícius Carneiro <vinixiussouza@gmail.com>
2022-12-15 18:20:12 -0300

    feat: use FastAPI

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import Union
+from fastapi import FastAPI
+
+from pipeline.predict import predict_quality
+
+app = FastAPI(title='Water Quality')
+
+
+@app.post("/predict")
+def predict_water_quality(data: list):
+    try:
+        quality = predict_quality([data])
+        return {'quality': str(quality[0])}
+    except Exception:
+        return {'error': 'It was not possible to classify the water quality.'}
